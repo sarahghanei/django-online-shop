b3e2e7823bad0e64b655fe86732ce1070d253928
amirbigg <amirbig44@gmail.com>
2020-03-08 12:23:07 +0330

    add ep6

--- A/settings.py
+++ A/settings.py
@@ -58,7 +58,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'accounts.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',

ef4aea7c0140583121f0e35b87c8c4059f5dd1db
amirbigg <amirbig44@gmail.com>
2020-03-11 20:03:10 +0330

    add ep13

--- shop/admin.py
+++ shop/admin.py
@@ -7,6 +7,7 @@ class CategoryAdmin(admin.ModelAdmin):
 	list_display = ('name', 'slug')
 	prepopulated_fields = {'slug':('name',)}
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
 	list_display = ('name', 'price', 'available')
@@ -14,3 +15,9 @@ class ProductAdmin(admin.ModelAdmin):
 	list_editable = ('price',)
 	prepopulated_fields = {'slug':('name',)}
 	raw_id_fields = ('category',)
+	actions = ('make_available',)
+
+	def make_available(self, request, queryset):
+		rows = queryset.update(available=True)
+		self.message_user(request, f'{rows} updated')
+	make_available.short_description = 'make all available'

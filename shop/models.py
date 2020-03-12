6e04b771f14c364c69e44018589fb7c049a42dea
amirbigg <amirbig44@gmail.com>
2020-03-12 10:39:38 +0330

    add ep14

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 
@@ -31,3 +32,6 @@ class Product(models.Model):
 
 	def __str__(self):
 		return self.name
+
+	def get_absolute_url(self):
+		return reverse('shop:product_detail', args=[self.slug,])

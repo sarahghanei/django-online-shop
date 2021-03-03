ed1d28c532a2a80a9557698d9c96367571430ea3
amirhossein bigdelu <amirbig44@gmail.com>
2021-03-03 04:09:30 -0500

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -22,7 +22,7 @@ def order_create(request):
 	order = Order.objects.create(user=request.user)
 	for item in cart:
 		OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
-		cart.clear()
+	cart.clear()
 	return redirect('orders:detail', order.id)
 
 
